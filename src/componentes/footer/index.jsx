524b034490452a9fc9de7abbc88767eca2ebc0e5
santiago vasquez <santito2004@hotmail.es>
2023-04-17 20:20:43 -0500

    agregando mis redes

--- src/componentes/footer/index.jsx
+++ src/componentes/footer/index.jsx
@@ -3,13 +3,13 @@ import "./footer.css"
 const Footer = () => {
     return <footer className="footer" style={{backgroundImage: "url(/img/footer.png)"}}>
         <div className="redes">
-            <a href="https://twitter.com/home">
+            <a href="https://twitter.com/vankisito">
                 <img src="/img/twitter.png" alt="Twitter"></img>
             </a>
-            <a href="https://www.facebook.com/">
+            <a href="https://www.facebook.com/santiago.vasquez.503645">
                 <img src="/img/facebook.png" alt="Face"></img>
             </a>
-            <a href="https://www.instagram.com/">
+            <a href="https://www.instagram.com/santiv04_/">
                 <img src="/img/instagram.png" alt="Insta"></img>
             </a>
         </div>
